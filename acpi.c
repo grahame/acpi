0538d59ea7d8064b97bb68b99731015a838007f9
meskes <meskes>
2009-02-11 09:45:44 +0000

    more re-formatting

--- acpi.c
+++ acpi.c
@@ -1,23 +1,22 @@
-/* provides a simple client program that reads ACPI status from the /proc 
-* filesystem
-*
-* Copyright (C) 2001  Grahame Bowland <grahame@angrygoats.net>
-*	    (C) 2008-2009  Michael Meskes  <meskes@debian.org>
-*
-*  This program is free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  This program is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  You should have received a copy of the GNU General Public License
-*  along with this program; if not, write to the Free Software
-*  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+/* provides a simple client program that reads ACPI status from the /proc filesystem
+ *
+ * Copyright (C) 2001  Grahame Bowland <grahame@angrygoats.net>
+ *	    (C) 2008-2009  Michael Meskes  <meskes@debian.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 #include <unistd.h>
 #include <sys/types.h>
@@ -31,7 +30,7 @@
 #include "list.h"
 #include "acpi.h"
 
-#define DEVICE_LEN	  20
+#define DEVICE_LEN	20
 #define BATTERY_DESC	"Battery"
 #define AC_ADAPTER_DESC "AC Adapter"
 #define THERMAL_DESC	"Thermal"
